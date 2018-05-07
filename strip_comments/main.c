df75b5a31500d532ed1d0cd44441f60e10c53374
lw <me@lw.ag>
2018-05-07 18:24:15 +0200

    Minor style change.

--- strip_comments/main.c
+++ strip_comments/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
-void handle_char(
-	char cur, char next, char *prev, int *lncmt, int *block, int *in_qts
-)
+
+void handle_line(char **, int *);
+void handle_char(char, char, char *, int *, int *, int *);
+
+int main()
+{
+	size_t n = 0;
+	char *buf = 0;
+
+	int block = 0;
+
+	while (getline(&buf, &n, stdin) != -1) {
+		handle_line(&buf, &block);
+	}
+}
+
+void handle_line(char **line, int *block)
+{
+	int lncmt = 0;		/* in a line comment */
+	int in_qts = 0;		/* in a quoted string */
+	char *ln = *line;
+	char cur, prev, next;
+
+	while ((cur = *ln++) != '\0') {
+		next = *ln;
+
+		handle_char(cur, next, &prev, &lncmt, block, &in_qts);
+	}
+}
+
+void handle_char(char cur, char next, char *prev, int *lncmt, int *block,
+	int *in_qts)
 {
 	if (cur == '"' && (!(*prev) || *prev != '\\'))
 		*in_qts = !(*in_qts);
@@ -24,30 +53,4 @@ void handle_char(
 	}
 
 	putchar(cur);
-}
-
-int handle_line(char **line, int *block)
-{
-	int lncmt = 0;		/* in a line comment */
-	int in_qts = 0;		/* in a quoted string */
-	char *ln = *line;
-	char cur, prev, next;
-
-	while ((cur = *ln++) != '\0') {
-		next = *ln;
-
-		handle_char(cur, next, &prev, &lncmt, block, &in_qts);
-	}
-}
-
-int main()
-{
-	size_t n = 0;
-	char *buf = 0;
-
-	int block = 0;
-
-	while (getline(&buf, &n, stdin) != -1) {
-		handle_line(&buf, &block);
-	}
 }
