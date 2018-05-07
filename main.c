cff012f8fc48b2fbe1e149cc7e04c8712e3a16cb
lw <me@lw.ag>
2018-05-07 18:07:56 +0200

    Expand with handle_char method.

--- main.c
+++ main.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
 
+void handle_char(
+	char cur, char next, char *prev, int *lncmt, int *block, int *in_qts
+)
+{
+	if (cur == '"' && (!(*prev) || *prev != '\\'))
+		*in_qts = !(*in_qts);
+
+	if (!(*block) && !(*in_qts) && cur == '/' && next == '*')
+		*block = 1;
+
+	if (!(*in_qts) && cur == '/' && next == '/')
+		*lncmt = 1;
+	if (!(*in_qts) && cur == '\n' && *lncmt)
+		*lncmt = 0;
+
+	if (*lncmt || *block) {
+		if (!(*in_qts) && cur == '/' && *prev == '*')
+			*block = 0;
+
+		*prev = cur;
+		return;
+	}
+
+	putchar(cur);
+}
+
+int handle_line(char **line, int *block)
+{
+	int lncmt = 0;		/* in a line comment */
+	int in_qts = 0;		/* in a quoted string */
+	char *ln = *line;
+	char cur, prev, next;
+
+	while ((cur = *ln++) != '\0') {
+		next = *ln;
+
+		handle_char(cur, next, &prev, &lncmt, block, &in_qts);
+	}
+}
+
 int main()
 {
-	double v, sum = 0;
+	size_t n = 0;
+	char *buf = 0;
 
-	while (scanf("%lf", &v) == 1)
-		printf("\t%f\n", sum += v);
+	int block = 0;
 
-	return 0;
+	while (getline(&buf, &n, stdin) != -1) {
+		handle_line(&buf, &block);
+	}
 }
