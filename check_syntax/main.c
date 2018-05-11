d686e43e395f8076930b0b290024dd82e91968ef
lw <me@lw.ag>
2018-05-11 22:05:27 +0200

    Implement comment handling.

--- check_syntax/main.c
+++ check_syntax/main.c
@@ -9,6 +9,7 @@ int main()
 	int par = 0;	// open paratheses
 	int brc = 0;	// open braces
 	int brk = 0;	// open brackets
+
 	int bcmt = 0;	// currently in a block comment
 
 	size_t n = 0;
@@ -21,22 +22,38 @@ int main()
 		sqt, dqt, par, brc, brk);
 
 	if ((sqt + dqt + par + brc + brk) != 0)
-		printf("a quote, brace, bracket or paranthesis was not closed");
-	else
-		ptinf("everything appears to be in order");
+		printf("a quote, brace, bracket or paranthesis was not closed\n");
 }
 
 void handle_line(char **line, int *sqt, int *dqt, int *par, int *brc, int *brk,
 	int *bcmt)
 {
+	int lcmt = 0;
+
 	char *ln = *line;
 	char cur, prev, next;
 
 	while ((cur = *ln++) != '\0') {
 		next = *ln;
-		prev = *(ln - 2); 
+		prev = *(ln - 2);
 
 		if (prev == '\\')
+			continue;
+
+		if (cur == '*' && prev == '/')
+			*bcmt = 1;
+		if (cur == '/' && prev == '*')
+			*bcmt = 0;
+		
+		if (*bcmt)
+			continue;
+
+		if (cur == '/' && prev == '/')
+			lcmt = 1;
+		if (lcmt && cur == '\n')
+			lcmt = 0;
+
+		if (lcmt)
 			continue;
 
 		switch (cur) {
@@ -46,6 +63,12 @@ void handle_line(char **line, int *sqt, int *dqt, int *par, int *brc, int *brk,
 		case '"':
 			*dqt += prev != '\'' ? (*dqt % 2 == 0) ? 1 : -1 : 0;
 			break;
+		}
+
+		if (*sqt % 2 == 1 || *dqt % 2 == 1)
+			continue;
+
+		switch (cur) {
 		case '(': (*par)++;
 			break;
 		case ')': (*par)--;
@@ -57,8 +80,6 @@ void handle_line(char **line, int *sqt, int *dqt, int *par, int *brc, int *brk,
 		case '[': (*brk)++;
 			break;
 		case ']': (*brk)--;
-			break;
-		default:
 			break;
 		}
 	}
