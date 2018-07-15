34cb587d7857443793715798f656cf39a6c3041b
lw <me@lw.ag>
2018-07-15 05:44:28 +0200

    Play around with signature scanning.

--- win_sigscan/main.c
+++ win_sigscan/main.c
@@ -7,39 +7,14 @@
 #define READ_SIZE 4096
 #define SIGNATURE "\xDB\x5D\xE8\x8B\x45\xE8\xA3"
 
-DWORD get_process_id(char *name);
-void *find_address(HANDLE *process, char *signature);
+int get_process_id(const char *name);
 
 int main()
 {
-	DWORD game_proc_id;
-	if (!(game_proc_id = get_process_id("osu!.exe"))) {
-		printf("error: couldn't find process");
-		return EXIT_FAILURE;
-	}
 
-	printf("found process with id %ld\n", game_proc_id);
-
-	HANDLE game_proc = NULL;
-	if (!(game_proc = OpenProcess(PROCESS_VM_READ, 0, game_proc_id))) {
-		printf("error: failed to get handle to process\n");
-		return EXIT_FAILURE;
-	}
-
-	printf("got handle to process\n");
-
-	void *time_address = NULL;
-	if (!(time_address = find_address(&game_proc, SIGNATURE))) {
-		printf("error: couldn't find address\n");
-		return EXIT_FAILURE;
-	}
-
-	printf("found time address %#x", time_address);
-
-	return 0;
 }
 
-DWORD get_process_id(char *name)
+int get_process_id(const char *name)
 {
 	DWORD proc_id;
 	HANDLE proc_list = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -62,44 +37,4 @@ DWORD get_process_id(char *name)
 	CloseHandle(proc_list);
 
 	return proc_id;
-}
-
-void *find_address(HANDLE *process, char *signature)
-{
-	bool found = true;
-	const size_t read_size = READ_SIZE;
-	const size_t sig_size = sizeof(signature);
-
-	size_t read;
-	int8_t *chunk;
-
-	for (size_t i = 0; i < INT_MAX; i += read_size) {
-		chunk = malloc(read_size);
-
-		if (!(ReadProcessMemory(*process, (void *)i, chunk, read_size,
-			&read)))
-		{
-			printf("error: failed to read chunk at %#x", i);
-			printf(" (error: %d, %d read)\n", GetLastError(), read);
-
-			continue;
-		}
- 
-		printf("read chunk at %#x (%d read)\n", i, i, read);
-
-		for (size_t j = 0; j < read_size; j++) {
-			found = true;
-
-			for (size_t k = 0; k < sig_size; k++) {
-				if (chunk[k + j] != signature[k])
-					found = false;
-			}
-
-			if (found) {
-				return (void *)(i + j);
-			}
-		}
-	}
-
-	return 0;
 }
