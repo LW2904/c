79374ffcdff1de2da2567857bd3e6bad7b26f377
lw <me@lw.ag>
2018-07-17 23:53:51 +0200

    Play around with standard deviations and such.

--- random_numbers/main.c
+++ random_numbers/main.c
@@ -1,13 +1,55 @@
+#include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RANGE 40
+#define ITERS 1000
+
+int generate_number(int d);
+float get_standard_dev(int count, int *data);
+
 int main()
 {
-	int i, r;
+	int rn, numbers[RANGE] = {0};
 
-	for (i = 0; i < 10; i++) {
-		r = (rand() % 20) - 10;
+	srand((unsigned) time(NULL));
 
-		printf("%d\n", r);
+	for (int i = 0; i < ITERS; i++) {
+		numbers[generate_number(RANGE)]++;
 	}
+
+	for (int i = 0; i < RANGE; i++) {
+		printf("%3d: %d\n", i - (RANGE / 2), numbers[i]);
+	}
+
+	int unstbl = get_standard_dev(RANGE, numbers) * 10;
+	printf("unstableness: %d\n", unstbl);
+
+	return 0;
+}
+
+int generate_number(int d)
+{
+	int ra = (rand() % d);
+	int rb = (rand() % (d / 2)) * ((ra < (d / 2)) ? -1 : 1);
+
+	return (ra + rb) % d;
+}
+
+float get_standard_dev(int count, int *data)
+{
+	int sum = 0;
+	float mean, sd = 0.0;
+
+	for (int i = 0; i < count; i++) {
+		sum += data[i];
+	}
+
+	mean = sum/10;
+
+	for(int i = 0; i < count; i++)
+		sd += pow(data[i] - mean, 2);
+
+	return sqrt(sd / 10);
 }
