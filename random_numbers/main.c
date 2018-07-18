98696859542d52cbbbcc19c009a3645faef0ef22
lw <me@lw.ag>
2018-07-19 00:14:52 +0200

    Refactor implementation to use rounds.

--- random_numbers/main.c
+++ random_numbers/main.c
@@ -2,12 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define RANGE 40
 #define ITERS 1000
 
-int generate_number(int d);
 float get_standard_dev(int count, int *data);
+static inline int is_in(int x, int from, int to);
+int generate_number(int range, int rounds, float bound);
 
 int main()
 {
@@ -16,7 +18,7 @@ int main()
 	srand((unsigned) time(NULL));
 
 	for (int i = 0; i < ITERS; i++) {
-		numbers[generate_number(RANGE)]++;
+		numbers[generate_number(RANGE, 2, 0.5)]++;
 	}
 
 	for (int i = 0; i < RANGE; i++) {
@@ -29,12 +31,27 @@ int main()
 	return 0;
 }
 
-int generate_number(int d)
+int generate_number(int range, int rounds, float bound)
 {
-	int ra = (rand() % d);
-	int rb = (rand() % (d / 2)) * ((ra < (d / 2)) ? -1 : 1);
+	int rn;
+	float minr = 0.5 - (bound / 2);
+	float maxr = 0.5 + (bound / 2);
 
-	return (ra + rb) % d;
+	rn = rand() % range;
+
+	for (int i = 0; i < rounds; i++) {
+		bool in = rn > (range * minr) && rn < (range * maxr);
+
+		rn += (in ? (rand() % (int)(range * minr)) : 0)
+			* (rn < (range * 0.5) ? -1 : 1);
+	}
+
+	return rn;
+}
+
+static inline int is_in(int x, int from, int to)
+{
+	return x > from && x < to;
 }
 
 float get_standard_dev(int count, int *data)
