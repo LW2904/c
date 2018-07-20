90044b39c5e533d26b1033ad3bc652bfaec3e39b
lw <me@lw.ag>
2018-07-20 23:36:17 +0200

    Refactoring.

--- vt100/main.c
+++ vt100/main.c
@@ -5,35 +5,48 @@
 
 #define DRAW_CHAR '#'
 
-#define ENMY_W 10
-#define ENMY_H 10
 
-void clear_screen(void);
-void print_message(char *msg);
-void move_cursor(int x, int y);
-void draw_vertical_line(short length);
-void draw_horizontal_line(short length);
-void draw_enemy(short width, short height);
-void move_cursor_by(short delta_x, short delta_y);
+// void print_message(char *msg);
+// void move_cursor(int x, int y);
+// void draw_vertical_line(short length);
+// void draw_horizontal_line(short length);
+// void draw_enemy(short width, short height);
+// void move_cursor_by(short delta_x, short delta_y);
+
+/**
+ * screen_ functions operate on the terminal as one construct.
+ */
+void screen_clear(void);
+
+/**
+ * cursor_ functions, related to movement of the cursor.
+ */
+void cursor_move(int x, int y, int back);
+void cursor_move_by(int delta_x, int delta_y);
+
+/**
+ * Utility functions.
+ */
+static inline int constrain_above(int orig, int min);
 
 int cur_x;
 int cur_y;
 
-int term_height;
-
 int main()
 {
+	// Disable stdout buffering.
 	setbuf(stdout, NULL);
 
+	// 0/0 is home.
 	cur_x = cur_y = 0;
 
 	int i = 0;
 	while (++i) {
-		clear_screen();
+		screen_clear();
 
-		move_cursor_by(i + ENMY_W, (i * 2) + ENMY_H);
+		cursor_move_by(i, i);
 
-		draw_enemy(ENMY_W, ENMY_H);
+		cursor_move(0, 0, 1);
 
 		sleep(1);
 	}
@@ -41,42 +54,52 @@ int main()
 	return 0;
 }
 
-void clear_screen()
+void screen_clear()
 {
 	printf("\e[2J");
 
-	move_cursor(0, 0);
+	cursor_move(0, 0, 0);
 }
 
-void move_cursor(int x, int y)
+void cursor_move(int x, int y, int back)
 {
-	if (x < 0 || y < 0) print_message("constraint error\n");
+	static int old_x = 0;
+	static int old_y = 0;
 
-	if (x < 0) x = 0;
-	if (y < 0) y = 0;
+	if (back) {
+		printf("%d / %d", old_x, old_y);
+		return cursor_move(old_x, old_y, 0);
+	}
 
-	cur_x = x;
-	cur_y = y;
+	old_x = x;
+	old_y = y;
+
+	cur_x = constrain_above(x, 0);
+	cur_y = constrain_above(y, 0);
 
 	printf("\e[%d;%dH", x, y);
-
-	// char *buf = malloc(256);
-	// sprintf(buf, "\e[%d;%dH\n", x, y);
-
-	// puts(buf);
-	// free(buf);
 }
 
-void move_cursor_by(short delta_x, short delta_y)
+void cursor_move_by(int delta_x, int delta_y)
 {
-	move_cursor(cur_x + delta_x, cur_y + delta_y);
+	cursor_move(cur_x + delta_x, cur_y + delta_y, 0);
 }
 
-void draw_enemy(short width, short height)
+void status_pos_update()
 {
-	int old_x = cur_x, old_y = cur_y;
 
-	draw_vertical_line(height + 1);
+}
+
+static inline int constrain_above(int orig, int min)
+{
+	return orig < min ? min : orig;
+}
+
+// void draw_enemy(short width, short height)
+// {
+// 	int old_x = cur_x, old_y = cur_y;
+
+// 	draw_vertical_line(height + 1);
 
 	// move_cursor_by((width / 2) * -1, (height / 2) * -1);
 
@@ -94,40 +117,40 @@ void draw_enemy(short width, short height)
 
 	// draw_line(width);
 
-	// move_cursor(old_x, old_y);
-}
+// 	// move_cursor(old_x, old_y);
+// }
 
-void draw_horizontal_line(short length)
-{
-	int old_x = cur_x, old_y = cur_y;
+// void draw_horizontal_line(short length)
+// {
+// 	int old_x = cur_x, old_y = cur_y;
 
-	for (int i = 0; i < length; i++) {
-		putchar(DRAW_CHAR);
-	}
+// 	for (int i = 0; i < length; i++) {
+// 		putchar(DRAW_CHAR);
+// 	}
 
-	move_cursor(old_x, old_y);
-}
+// 	move_cursor(old_x, old_y);
+// }
 
-void draw_vertical_line(short length)
-{
-	int old_x = cur_x, old_y = cur_y;
+// void draw_vertical_line(short length)
+// {
+// 	int old_x = cur_x, old_y = cur_y;
 
-	for (int i = 0; i < length; i++) {
-		putchar(DRAW_CHAR);
+// 	for (int i = 0; i < length; i++) {
+// 		putchar(DRAW_CHAR);
 
-		move_cursor_by(0, (-1));
-	}
+// 		move_cursor_by(0, (-1));
+// 	}
 
-	move_cursor(old_x, old_y);	
-}
+// 	move_cursor(old_x, old_y);	
+// }
 
-void print_message(char *msg)
-{
-	int old_x = cur_x, old_y = cur_y;
+// void print_message(char *msg)
+// {
+// 	int old_x = cur_x, old_y = cur_y;
 	
-	move_cursor(0, 0);
+// 	move_cursor(0, 0);
 
-	printf("%s", msg);
+// 	printf("%s", msg);
 
-	move_cursor(old_x, old_y);	
-}
+// 	move_cursor(old_x, old_y);	
+// }
