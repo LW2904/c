50353315e312a4edc949b2e253cf1165eba8cfb2
lw <me@lw.ag>
2018-08-16 01:03:12 +0200

    Remove obsolete includes.

--- x11_window_title/main.c
+++ x11_window_title/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <X11/X.h>
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xatom.h>
-#include <X11/XKBlib.h>
-#include <X11/keysym.h>
-#include <X11/Xresource.h>
-#include <X11/cursorfont.h>
-#include <X11/extensions/XTest.h>
-#include <X11/extensions/Xinerama.h>
 
 Display *display;
 
